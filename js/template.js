8ed2cb5bcb560da47f3d2b5e6744bc24f23e81a6
garydot <gary.dot@gmail.com>
2011-11-17 19:04:10 +0800

    update

--- js/template.js
+++ js/template.js
@@ -1,16 +1,40 @@
 /**
  * @author gary.zhan
  */
+
+/**
+ * class template
+ */
 ( function(ctx) {
 	//Constructor
 	var SomeClass = ctx.SomeClass = function() {
-		//todo:constructor
+		//public constructor
+		this.property1 = "";
+		this.property2 = 0;
 	}
-
-	SomeClass.prototype = {
-		init:function(jsonUrl) {
-			//todo:init
-		}
+	//private function
+	function somePrivateFunc() {
 	}
 	
+	//private property
+	var privateVal = 0;
+
+	SomeClass.prototype = {
+		init : function(jsonUrl) {
+			//todo:init
+		},
+		somePublicFunc : function(jsonUrl) {
+		}
+	}
 }(JS));
+
+/**
+ * app template
+ */
+//todo init 
+var main = JS.main = function() {
+
+}
+$(document).ready(function() {
+	JS.main();
+});
